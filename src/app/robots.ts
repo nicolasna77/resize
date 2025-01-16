d49b1e0a762b3c728641394df937ac5ec8e70353
nicolasna77 <nicolas_abreu@live.fr>
2025-01-16 14:53:22 +0100

    add robot google

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: "/",
       disallow: "/api/",
     },
-    sitemap: "https://votre-domaine.com/sitemap.xml",
+    sitemap: "https://resize2.vercel.app/sitemap.xml",
   };
 }
--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = "https://resize.vercel.app";
+  const baseUrl = "https://resize2.vercel.app";
 
   return [
     {
